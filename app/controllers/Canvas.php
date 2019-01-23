8b0d25b84e19a10cdf038049e14c45a1596e95bb
Evan Sharp <ego@evansharp.ca>
2019-01-23 11:57:23 -0800

    a

--- app/controllers/Canvas.php
+++ app/controllers/Canvas.php
@@ -1,10 +1,10 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Canvas extends MY_controller {
+class Canvas extends MY_controller{
 
 	public function __construct(){
-
+		parent::__construct();
 	}
 	public function index(){}
 
--- app/core/MY_controller.php
+++ app/core/MY_controller.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class MY_controller extends CI_Controller{
 
     public function __construct(){
-
+        parent::__construct();
     }
 
 }
