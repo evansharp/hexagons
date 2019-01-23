20cf0a3fb00ae7780f3dd9bda5eb2ba1d6e4f7d2
Evan Sharp <ego@evansharp.ca>
2019-01-23 12:02:19 -0800

    Update Canvas.php

--- app/controllers/Canvas.php
+++ app/controllers/Canvas.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Canvas extends My_Controller{
+class Canvas extends MY_Controller{
 
 	public function __construct(){
 		parent::__construct();
