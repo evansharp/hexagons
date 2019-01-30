f90e7bafb328a105a9bbb5aa14769566f5cc4ed6
Evan Sharp <ego@evansharp.ca>
2019-01-29 20:26:20 -0800

    color change and trash now work

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,4 +1,4 @@
-
+var canvas = {};
 
 function loading(){
     var i = M.Modal.getInstance( document.querySelector('#loader_modal') );
@@ -14,18 +14,72 @@ fabric.Canvas.prototype.getAbsoluteCoords = function(object) {
   };
 }
 
+function attachListeners(){
+    // init just-added color wheel controls
+    $('.colorwheel').colorwheel();
+
+    $('.colorwheel_handle').click(function(e){
+        e.preventDefault();
+        $(this).prev('.colorwheel').fadeToggle(100);
+    });
+
+    //when a colour is picked...
+    $('.colorwheel').click(function(e){
+        e.preventDefault();
+        var color = new fabric.Color('#' + $(this).colorwheel('value') );
+        var id = $(this).parent('.hex_controls').attr('data-hex-id');
+        var hex = canvas.getObjects().filter(function(o) {
+            if (o.id == id) {
+                return o;
+            }
+        });
+
+        hex[0].set('fill', '#' + $(this).colorwheel('value') );
+        $(this).fadeOut(100);
+        canvas.renderAll();
+    });
+
+    //when a trashcan is kicked...
+    $('.trashcan').click(function(e){
+        e.preventDefault();
+        var controls = $(this).parent('.hex_controls');
+        var hex = canvas.getObjects().filter(function(o) {
+            if (o.id == controls.attr('data-hex-id') ) {
+                return o;
+            }
+        });
+
+        hex[0].remove();
+        controls.remove();
+    });
+
+    $('.labelmaker').click(function(e){
+        e.preventDefault();
+        var id = $(this).parent('.hex_controls').attr('data-hex-id');
+
+        var html = '<input type="text" class="" data-hex-id=>';
+        $( html ).appendTo('#texts');
+    });
+}
+
 function positionCtl( canvas, obj, ctls ){
     var absCoords = canvas.getAbsoluteCoords( obj );
     ctls.style.left = (absCoords.left + 24) + 'px';
     ctls.style.top = (absCoords.top + 1) + 'px';
 }
 
+function positionText( canvas, obj, text ){
+    var absCoords = canvas.getAbsoluteCoords( obj );
+    ctls.style.left = (absCoords.left) + 'px';
+    ctls.style.top = (absCoords.top + 20) + 'px';
+}
+
 function getControlGroupString(id){
     return '<div class="hex_controls" data-hex-id="' + id + '">' +
                 '<div class="colorwheel"></div>'+
                 '<a href="" class="hex_control colorwheel_handle"><i class="material-icons">color_lens</i></a>'+
-                '<a href="" class="hex_control"><i class="material-icons">delete</i></a>'+
-                '<a href="" class="hex_control"><i class="material-icons">text_fields</i></a>'+
+                '<a href="" class="hex_control trashcan"><i class="material-icons">delete</i></a>'+
+                '<a href="" class="hex_control labelmaker"><i class="material-icons">text_fields</i></a>'+
             '</div>';
 }
 
@@ -37,7 +91,7 @@ $(document).ready(function(){
     $('.tooltipped').tooltip();
     $('.modal').modal();
 
-    var canvas = new fabric.Canvas('c',{
+    canvas = new fabric.Canvas('c',{
         height:	window.innerHeight - 90,
         width: window.innerWidth - 20
     });
@@ -45,21 +99,28 @@ $(document).ready(function(){
     if( canvasData ){
         //process canvas objects to add settings not exported properly
         var rough_canvas = JSON.parse( canvasData );
+
         $.each(rough_canvas.objects, function(i, canvas_item){
             canvas_item.hasBorders = false;
             canvas_item.hasControls = false;
             canvas_item.hasRotatingPoint = false;
-            canvas_item.id = hex_id_counter; //restore an idea for control group
-            $('#controls').append( getControlGroupString( hex_id_counter) ); //create control group
+            canvas_item.id = hex_id_counter; //restore an id for control group
+
             hex_id_counter += 1; //increment counter for the next hexagon
         });
-        console.log(rough_canvas);
+
         //stupid Fabric JS won't import an object...
         var fine_canvas = JSON.stringify( rough_canvas );
         canvas.loadFromJSON( fine_canvas );
 
-        // init just-added color wheel controls
-        $('.colorwheel').colorwheel();
+        //attach controls to each hexagon now that it's on the canvas
+        $.each(canvas.getObjects(), function( i, o ){
+            var new_controls = $( getControlGroupString( o.id ) ).appendTo('#controls').get(0);
+            o.on('moving', function() { positionCtl( canvas, o, new_controls) });
+            positionCtl( canvas, o, new_controls);
+        });
+
+        attachListeners();
     }
 
     // var grid = 120;
@@ -153,21 +214,11 @@ $(document).ready(function(){
 
         var new_controls = $( getControlGroupString( hex_id_counter) ).appendTo('#controls').get(0);
         $('.colorwheel').colorwheel();
-
         path.on('moving', function() { positionCtl( canvas, path, new_controls) });
-
         positionCtl( canvas, path, new_controls);
 
+        attachListeners();
         hex_id_counter += 1; //increment for next new hexagon
-
-        $('.colorwheel_handle').click(function(e){
-            e.preventDefault();
-            $(this).prev('.colorwheel').fadeToggle(100);
-        });
-        $('.colorwheel').click(function(){
-            console.log( '#' + $(this).colorwheel('value') );
-            $(this).hide();
-        });
     });
 
 
